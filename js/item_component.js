944f6761934cee6b583565db1a00b65347d62c05
Axel <adalvarez@scl.edu.gt>
2023-03-18 16:09:30 -0600

    Ya casi

--- js/item_component.js
+++ js/item_component.js
@@ -3,7 +3,7 @@ export function  item_component(numero_tarea){
     let  item=  `
     <div class="todo_item">
         <span class="ico">✅</span>
-            <p>
+            <p class="tarea${numero_tarea}">
                 ${data_todo_list[numero_tarea].mensaje}
             </p>
         <span class="ico2">❎</span>
